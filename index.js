71c30c7781da568b387fa923fa9df78dbd995ee6
Max Houston Ramirez Martel <maxr523@gmail.com>
2020-09-28 22:19:46 -0500

    configuraciones

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-const brain = require('brain.js');
+const brain = require("brain.js");
 const network = new brain.recurrent.LSTM();
-const tdata = require('./data.json');
+const tdata = require("./data.json");
 network.fromJSON(tdata);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
-app.get('/evaluar/:texto', (req, res) => {
-    const texto = req.params.texto;
-    const result = network.run(texto.trim());
+app.get("/evaluar/:texto", (req, res) => {
+  const texto = req.params.texto;
+  const result = network.run(texto.trim());
 
-    res.json({
-        input: texto,
-        output: result
-    });
+  res.json({
+    input: texto,
+    output: result,
+  });
 });
 
-app.listen(port, () => {
-    console.log('Server on port ' + port);
+app.listen(PORT, () => {
+  console.log("Server on PORT " + PORT);
 });
 //holas
